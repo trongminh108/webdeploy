1d1c8d4931051191ea797d5e17e4deff98edd47c
trong minh <lmtrong.ctc2020@gmail.com>
2023-08-26 00:24:51 +0700

    fix: fix slug 5

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
     output: 'export',
     basePath: '/webdeploy',
+    pageExtensions: [
+        'page.tsx',
+        'page.ts',
+        'page.jsx',
+        'page.js',
+        'page.md',
+        'page.mdx',
+    ],
 };
 
 module.exports = nextConfig;
