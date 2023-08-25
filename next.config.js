4449845d15cc9423285b34913c7cd2b1bc0a760a
trong minh <lmtrong.ctc2020@gmail.com>
2023-08-26 01:13:38 +0700

    fix: fix slug 11

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/webdeploy',
-    experimental: {
-        runtime: 'experimental-edge',
-        appDir: true,
-    },
-    trailingSlash: true,
 };
 
 module.exports = nextConfig;
