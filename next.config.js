cfdbb32b83a9f7b07997c1a1ee60d2c9f7902c2d
trong minh <lmtrong.ctc2020@gmail.com>
2023-08-26 11:58:00 +0700

    fix: fix slug 24

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
+    basePath: '/webdeploy',
+    assetPrefix: '/webdeploy/',
     experimental: {
         appDir: true,
     },
