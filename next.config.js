65797b107e42d0fb26a955c964d317227ccc0c54
trong minh <lmtrong.ctc2020@gmail.com>
2023-08-26 09:10:05 +0700

    fix: fix slug 18

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    // output: 'export',
+    output: {
+        dir: 'out',
+    },
     experimental: {
         appDir: true,
     },
